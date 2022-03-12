14eaafbb7cb5257cce40a327808c4c3bdbe27df6
Michael Schletz <schletz@spengergasse.at>
2022-03-12 11:38:35 +0100

    Commit 20220312

---
+++ qr_decoder/src/index.js
@@ -0,0 +1,20 @@
+import base45 from 'base45-web'
+import pako from 'pako'
+import { Html5QrcodeScanner } from "html5-qrcode"
+import { Decoder } from 'cbor-web'
+import { Buffer } from 'Buffer'
+
+function decode(base45String) {
+    const match = /HC1\:(?<data>[ \$\%\*\+\-\.\/\:0-9A-Z]+)/.exec(base45String);
+    if (!match) { throw "Invalid healthcare code or invalid base45 string."; }
+    const decodedData = Buffer.from(base45.decode(match.groups.data));
+    const unzipped = pako.inflate(decodedData);
+    const results = Decoder.decodeAllSync(unzipped);
+    const [headers1, headers2, cbor_data, signature] = results[0].value;
+    return cbor_data;
+}
+
+export {
+    decode,
+    Html5QrcodeScanner
+};
