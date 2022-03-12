78721d080e8a20e617f69cfc618b2714eea193f6
Michael Schletz <schletz@spengergasse.at>
2022-03-12 13:22:04 +0100

    Commit 20220312

--- Specials/QrDecoder/src/index.js
+++ Specials/QrDecoder/src/index.js
@@ -5,12 +5,14 @@ import { Decoder } from 'cbor-web'
 import { Buffer } from 'Buffer'
 
 function decode(base45String) {
+
     const match = /HC1\:(?<data>[ \$\%\*\+\-\.\/\:0-9A-Z]+)/.exec(base45String);
     if (!match) { throw "Invalid healthcare code or invalid base45 string."; }
     const decodedData = Buffer.from(base45.decode(match.groups.data));
     const unzipped = pako.inflate(decodedData);
-    const results = Decoder.decodeAllSync(unzipped);
-    const [headers1, headers2, cbor_data, signature] = results[0].value;
+    const result = Decoder.decodeFirstSync(unzipped);
+    const [headers1, headers2, cbor_data, signature] = result.value;
+    // https://dgc.a-sit.at/ehn
     return cbor_data;
 }
 
