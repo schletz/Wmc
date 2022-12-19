724793a1075162267f5fc9fdabbd25f3c775303c
Michael Schletz <schletz@spengergasse.at>
2022-12-19 08:23:56 +0100

    Infos about vue files.

--- 32_Vuejs/Spengernews.Client/src/components/NewsContent.vue
+++ 32_Vuejs/Spengernews.Client/src/components/NewsContent.vue
@@ -1,42 +1,54 @@
 <script setup>
-import axios from 'axios';
+import axios from "axios";
 </script>
 
 <template>
+  <div class="newsContent">
     <h3>{{ newsData.headline }}</h3>
     <p>{{ newsData.content }}</p>
     <p>
-        Crated at {{ new Date(newsData.created).toLocaleString('de-AT', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }) }} by {{ newsData.authorFirstname }}
-        {{ newsData.authorLastname }}
+      Crated at
+      {{
+        new Date(newsData.created).toLocaleString("de-AT", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      }}
+      by {{ newsData.authorFirstname }}
+      {{ newsData.authorLastname }}
     </p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        newsid: String,
+  props: {
+    newsid: String,
+  },
+  data() {
+    return {
+      newsData: {},
+    };
+  },
+  methods: {
+    async loadArticle(newsid) {
+      try {
+        this.newsData = (await axios.get(`news/${newsid}`)).data;
+      } catch (e) {
+        alert("Fehler beim Laden des Newscontents.");
+      }
     },
-    data() {
-        return {
-            newsData: {},
-        };
+  },
+  async mounted() {
+    await this.loadArticle(this.newsid);
+  },
+  watch: {
+    async newsid(newVal) {
+      await this.loadArticle(newVal);
     },
-    methods: {
-        async loadArticle(newsid) {
-            try {
-                this.newsData = (await axios.get(`news/${newsid}`)).data;
-            } catch (e) {
-                alert('Fehler beim Laden des Newscontents.');
-            }
-        },
-    },
-    async mounted() {
-        await this.loadArticle(this.newsid);
-    },
-    watch: {
-        async newsid(newVal) {
-            await this.loadArticle(newVal);
-        },
-    },
+  },
 };
 </script>
