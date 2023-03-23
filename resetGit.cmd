38fe040f2b77e1ad1bb1a585455e6fb81015642f
David Mazurek <98526080+maz22374@users.noreply.github.com>
2023-03-23 23:11:51 +0100

    Update resetGit.cmd

--- resetGit.cmd
+++ resetGit.cmd
@@ -1,16 +1,30 @@
 @echo off
 chcp 65001
-echo Achtung: Alle lokalen Änderungen werden zurückgesetzt. Drücke CTRL+C zum Abbrechen.
+echo Warning: All local changes will be reset. Press CTRL+C to cancel.
 pause
 
+REM Get the current branch
 FOR /F "tokens=*" %%a IN ('git branch --show-current') DO (SET current_branch=%%a)
 
-git fetch --all --prune
-FOR /F "tokens=* delims=* " %%a IN ('git branch') DO (
-    echo Reset branch %%a...
-    git checkout %%a > nul 2>&1
-    REM git clean -df
-    git reset --hard origin/%%a > nul 2>&1
+REM Check if there are any uncommitted changes
+SET has_changes=0
+FOR /F "tokens=* delims=* " %%a IN ('git status --porcelain') DO (
+    SET has_changes=1
 )
-git checkout %current_branch% > nul 2>&1
-echo You are in branch %current_branch%
+IF %has_changes% == 1 (
+    echo You have uncommitted changes. Please commit or stash them before running this script.
+    exit /b 1
+)
+
+REM Reset all branches
+FOR /F "tokens=*" %%a IN ('git branch') DO (
+    echo Resetting branch %%a...
+    git checkout %%a && git clean -df && git reset --hard origin/%%a || (
+        echo Failed to reset branch %%a.
+        exit /b 1
+    )
+)
+
+REM Go back to the current branch
+git checkout %current_branch%
+echo You are now on branch %current_branch%
